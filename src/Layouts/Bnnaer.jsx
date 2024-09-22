05b384c43c4c03567cc8c51bb311d176898045a5
sajibkhandev <sajibkhan.mern@gmail.com>
2024-09-22 16:34:52 +0600

    banner part done

--- src/Layouts/Bnnaer.jsx
+++ src/Layouts/Bnnaer.jsx
@@ -3,6 +3,7 @@ import Container from '../components/Container'
 import Flex from '../components/Flex'
 import BannerImage from '../assets/banner.png'
 import Button from '../components/Button'
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Bnnaer = () => {
   return (
@@ -10,12 +11,12 @@ const Bnnaer = () => {
         <Container>
             <Flex>
                 <div className='w-[50%]'>
-                    <h1 className='text-5xl text-white'>Building stellar websites for early startups</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    <h1 className='text-[54px] text-white font-pop font-semibold'>Building stellar websites for early startups</h1>
+                    <p className='text-base text-white font-pop font-normal w-[562px] pt-6 pb-12'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
 
-                    <div>
+                    <div className='flex relative'>
                     <Button text='View our work'/>
-                    <Button text='View our work'/>
+                     <div className='group text-white flex items-center absolute top-[16px] left-[255px]'><a  href="">View Pricing</a><FaArrowRightLong className='absolute top-[6px] left-[100px] opacity-0 group-hover:opacity-100'/></div>
                     </div>
                 </div>
                 <div className='w-[50%]'>
